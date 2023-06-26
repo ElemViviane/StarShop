a84ad57817277466c9b5d0cbba8514cbe9691cd1
Elem Viviane <131198461+ElemViviane@users.noreply.github.com>
2023-06-26 11:29:26 -0300

    Criando

---
+++ Compra.java
@@ -0,0 +1,31 @@
+package br.com.starshop.modelos;
+
+public class Compra implements Comparable<Compra> {
+
+	private String descricao;
+	private double valor;
+
+	public Compra(String descricao, double valor) {
+		this.descricao = descricao;
+		this.valor = valor;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	@Override
+	public String toString() {
+		return "Compra:\ndescrição: " + descricao + 
+				"\nvalor: " + valor + "\n";
+	}
+
+	@Override
+	public int compareTo(Compra outraCompra) {
+		return Double.valueOf(this.valor).compareTo(Double.valueOf(outraCompra.valor));
+	}	
+}
